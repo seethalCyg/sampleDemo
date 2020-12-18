2f81e52b466841bf45ba194165a10ff99ea387c6
seethalCyg <76089947+seethalCyg@users.noreply.github.com>
2020-12-18 17:34:37 +0530

    Update Example.java
    line 9 change mergetest

--- src/demo/Example.java
+++ src/demo/Example.java
@@ -4,7 +4,9 @@ public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("This is a sample project");
+     // System.out.println("This is a sample project");
+		// code for merge test
+     System.out.println("This is a sample project");
 	}
 
 }
