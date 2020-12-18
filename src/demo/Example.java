9164c55a178429d272e9aa558a5f1355002d3297
seethalCyg <76089947+seethalCyg@users.noreply.github.com>
2020-12-18 18:40:22 +0530

    Update Example.java
    test code line 7

--- src/demo/Example.java
+++ src/demo/Example.java
@@ -3,10 +3,9 @@ package demo;
 public class Example {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     // System.out.println("This is a sample project");
-		// code for merge test
-     System.out.println("This is a sample project");
+		// test  code for merge
+     System.out.println("This is a tester branch");
+		// TODO Auto-generated method stub)
 	}
 
 }
